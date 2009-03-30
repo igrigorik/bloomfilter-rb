d597bc2b511e883f82358f7181f6ca4cd4487350
postmodern <postmodern.mod3@gmail.com>
2009-03-30 03:15:02 -0700

    Properly decode the 5th argument of BloomFilter.new.
    * This solves the mysterious "TypeError: cannot convert false to Integer"
      exceptions.

--- ext/sbloomfilter.c
+++ ext/sbloomfilter.c
@@ -85,37 +85,42 @@ static VALUE bf_s_new(int argc, VALUE *argv, VALUE self) {
         arg2 = argv[1];
         arg3 = argv[2];
         arg4 = argv[3];
-        arg5 = argv[4];
+
+	if (TYPE(argv[4]) == T_TRUE) {
+	    arg5 = INT2FIX(1);
+	} else {
+	    arg5 = INT2FIX(0);
+	}
     } else if (argc == 4) {
         arg1 = argv[0];
         arg2 = argv[1];
         arg3 = argv[2];
         arg4 = argv[3];
-        arg5 = 0;
+        arg5 = INT2FIX(0);
     } else if (argc == 3) {
         arg1 = argv[0];
         arg2 = argv[1];
         arg3 = argv[2];
         arg4 = INT2FIX(1);
-        arg5 = 0;
+        arg5 = INT2FIX(0);
     } else if (argc == 2) {
         arg1 = argv[0];
         arg2 = argv[1];
         arg3 = INT2FIX(0);
         arg4 = INT2FIX(1);
-        arg5 = 0;
+        arg5 = INT2FIX(0);
     } else if (argc == 1) {
         arg1 = argv[0];
         arg2 = INT2FIX(4);
         arg3 = INT2FIX(0);
         arg4 = INT2FIX(1);
-        arg5 = 0;
+        arg5 = INT2FIX(0);
     } else { /* default = Fugou approach :-) */
         arg1 = INT2FIX(100000000);
         arg2 = INT2FIX(4);
         arg3 = INT2FIX(0);
         arg4 = INT2FIX(1);
-        arg5 = 0;
+        arg5 = INT2FIX(0);
     }
 
     m = FIX2INT(arg1);
